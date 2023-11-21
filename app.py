57347a34a817cf20e980c0262680ccc082e2f0b1
ikario <mvillanueva.a@outlook.com>
2023-11-20 22:40:10 -0600

    uso de columna tranc_Monto_F (negativos)

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 def remove_accents(df):
     for column in df.columns:
         strip_accents_udf = udf(lambda x: unicodedata.normalize('NFD', x).encode('ascii', 'ignore').decode('utf-8') if x is not None else x)
-        df = df.withColumn(column, strip_accents_udf(col(column)))
+        df = df.withColumn(column, strip_accents_udf(col(column))) 
     return df
    
 def rename_columns(df):
@@ -35,16 +35,16 @@ df = df.withColumn("monto_F_ABS", abs(df["tranc_Monto_F"]))
 # Reemplaza valores nulos o vacíos en la columna "cli_CODCLIENTE" con "000-000"
 df = df.withColumn("cli_CODCLIENTE", coalesce(df["cli_CODCLIENTE"], lit("000-000")))
 
-df = df.withColumn('Beneficios', df['monto_F_ABS'] - df['pro_Costo_del_articulo'])
-df = df.withColumn('Perdidas', when(df['pro_Costo_del_articulo'] - df['monto_F_ABS'] < 0, 0).otherwise(df['pro_Costo_del_articulo'] - df['monto_F_ABS']))
-df = df.withColumn('Rentabilidad', (df['Beneficios'] / df['monto_F_ABS']) * 100)
-df = df.withColumn('Ingresos_Generados', df['monto_F_ABS'] - df['tranc_Descuento_total'])
+df = df.withColumn('Beneficios', df['tranc_Monto_F'] - df['pro_Costo_del_articulo'])
+df = df.withColumn('Perdidas', when(df['pro_Costo_del_articulo'] - df['tranc_Monto_F'] < 0, 0).otherwise(df['pro_Costo_del_articulo'] - df['tranc_Monto_F']))
+df = df.withColumn('Rentabilidad', (df['Beneficios'] / df['tranc_Monto_F']) * 100)
+df = df.withColumn('Ingresos_Generados', df['tranc_Monto_F'] - df['tranc_Descuento_total'])
 df = df.withColumn('Margen_de_Beneficios', (df['Beneficios'] / df['Ingresos_Generados']) * 100)
-df = df.withColumn('Monto_Total_con_Descuento', df['monto_F_ABS'] - df['tranc_Descuento_total'])
+df = df.withColumn('Monto_Total_con_Descuento', df['tranc_Monto_F'] - df['tranc_Descuento_total'])
 df = df.withColumn("tranc_Fecha_de_contabilizacion", date_format(to_date("tranc_Fecha_de_contabilizacion", "dd-MM-yy"), "dd/MM/yyyy"))
 df = df.withColumn("tranc_QUANTITY_F", coalesce(round(col("tranc_QUANTITY_F"), 2), lit(0)))
 # Guarda el DataFrame transformado en un solo archivo CSV
-df.coalesce(1).write.csv(output_file, header=True, mode="overwrite", sep=",")
+df.coalesce(1).write.csv(output_file, header=True, mode="overwrite", sep=";")
 
 # Copia el último archivo generado a la carpeta /tmp/result con el nombre ventasprocesadas.csv
 latest_file = max(glob.glob(f"{output_file}/*.csv"), key=os.path.getctime)
